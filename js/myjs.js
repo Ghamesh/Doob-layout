bce447339938b6e37bb2ef3633cd08dcaea7cdc4
Ghamesh <ghamesh441@gmail.com>
2021-01-17 22:11:16 +0530

    complete layout

---
+++ js/myjs.js
@@ -0,0 +1,9 @@
+
+$('.toggle').click(function(){
+    $('.menu').slideToggle('fast', function(){
+          if($(this).is(':visible'));
+          $(this).css('display', 'flex');
+        });
+       
+      });
+    
